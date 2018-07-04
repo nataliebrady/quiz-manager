b45a683417b8e28344c2e7e2576e9fb122ad84b1
Natalie Brady <brady.visible@hotmail.com>
2018-07-04 09:50:09 +0000

    add changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require cocoon
 //= require jquery
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require cocoon
 //= require_tree .
