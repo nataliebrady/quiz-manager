d89fb45db9276ec86d0cc80584c8efb118e6b7ea
Natalie Brady <brady.visible@hotmail.com>
2018-07-28 13:05:39 +0100

    check if user has questions answered

--- app/controllers/answered_questions_controller.rb
+++ app/controllers/answered_questions_controller.rb
@@ -1,15 +1,16 @@
 class AnsweredQuestionsController < ApplicationController
-  
+
   def index
     @answered_questions = AnsweredQuestion.paginate(page: params[:page])   
   end
   
   def show
     @answered_questions = AnsweredQuestion.find(params[:id])
+    @answered_question = AnsweredQuestion.all
   end
 
   def create 
-    @answered_questions = AnsweredQuestion.new(answered_params)
+    @answered_questions = current_user.answered_questions.create(answered_params)
     if @answered_questions.save
       flash[:success] = "You have completed the quiz!"
       redirect_to @answered_questions
@@ -32,7 +33,7 @@ class AnsweredQuestionsController < ApplicationController
   private
   
   def answered_params
-    params.require(:answered_question).permit(:question_id, :answer_id, :user_id)
+    params.permit(:answered_question).permit(:question_id, :answer_id, :user_id, :question_title, :answer_title, :name)
   end
 
 end
